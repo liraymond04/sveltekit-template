698dd3dbbb7815fc0ff4ac6b3af6302630ca188f
Raymond Li <li.raymond04@gmail.com>
2025-08-05 12:19:33 -0600

    Add more TODO hints

--- src/routes/p/[id]/+page.svelte
+++ src/routes/p/[id]/+page.svelte
@@ -1,3 +1,17 @@
+<!-- TODO -->
+<!---->
+<!-- This is the front-end page component for our route -->
+<!---->
+<!-- Is there any data we need to use from our backend? -->
+<!---->
+<!-- If so, how should we access it? -->
+<!---->
+<!-- How might we want to style and compose the content on our page? -->
+<!---->
+<!-- Do we need any extra components or libraries to do this? -->
+<!---->
+<!-- https://tailwindcss.com/docs/ -->
+<!---->
 <script lang="ts">
   const { data } = $props();
 </script>
