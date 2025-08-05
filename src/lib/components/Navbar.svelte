63759222e59a21f8fd8762fbe4d3b7b99af8638a
Raymond Li <li.raymond04@gmail.com>
2025-08-05 09:26:37 -0600

    Add some template items

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav class="navbar bg-base-100 shadow-md px-4 relative">
+  <div class="flex-1 flex items-center">
+    <a class="btn btn-ghost text-xl" href="/">
+      <span class="text-2xl font-semibold">Home</span>
+    </a>
+  </div>
+</nav>
